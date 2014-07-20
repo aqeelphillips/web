288cdc4a27ef3874a91560aa7978b5708eb51971
Aqeel Phillips <indoninjah@gmail.com>
2014-07-20 10:25:31 -0400

    Moved Spark click event logic into home-view

--- js/idea-bucket.js
+++ js/idea-bucket.js
@@ -1,6 +1,9 @@
 var app = app || {};
 
 (function() {
+	//This should eventually be contained in the IdeaBucket view 
+	//(which is inside the Sidebar in side the HomeView view)
+	//Specifically in the Render method
 	app.IdeaBucket = {
 		jawnsInBucket : [],
 		addSpark : function(id) { 
--- js/views/home-view.js
+++ js/views/home-view.js
@@ -4,13 +4,14 @@ var app = app || {};
 	app.HomeView = Backbone.View.extend({
 		events: {
 			"click div.item.spark": "showDetail"
-		}
+		},
 
 		el: '#page',
 		render: function() {
 			var jawns = new app.Jawns();
 			jawns.fetch({
 				success: function(jawns) {
+					this.collection = jawns;
 					var template = _.template($("#home-view-template").html(), { jawns: jawns.models });
 					this.$el.html(template);
 
@@ -19,15 +20,87 @@ var app = app || {};
 							gutter: '.gutter-sizer',
 							itemSelector: '.item'
 						});
-						new app.SparkDataFiller(jawns);
+						//new app.SparkDataFiller(jawns);
 					}.bind(this));
 				}.bind(this)
 			});
 		},
 
 		//Spark Data should pretty much exclusively go in here:
-		showDetail: function() {
+		showDetail: function(e) {
+			e.preventDefault();
+	        var index = ($(e.currentTarget).index()) - 1;
+	        var litespark = this.collection.at(index);
+			var id = litespark.get('id');
 
+			var spark = new app.Spark();
+			spark.url = '/sparks/'+id+'.json';
+			spark.fetch({
+				success: function() {
+					console.log(spark);
+
+					var content = spark.get('content');
+					var content_type = "Text";
+					var body = '<p>'+content+'</p>';
+					var overlay_col = 'rgba(255, 255, 255, 0.4)';
+					switch (spark.get('content_type')) {
+						case 'P':
+							content_type = "Picture";
+							var file = spark.get('file');
+							body = '<img src="'+file+'" height="600px" width="600px"/>';
+							overlay_col = 'rgba(204, 255, 204, 0.3)';
+							break;
+						case 'V':
+							content_type = "Video";
+							//Assuming the content is a youtube video:
+							body = '<iframe class="youtube-player" type="text/html" width="600" height="400" src="http://www.youtube.com/v/'
+								+content.split('http://www.youtube.com/watch?v=')[1]
+								+'&autoplay=1" allowfullscreen frameborder="0"></iframe>';
+							overlay_col = 'rgba(255, 204, 204, 0.3)';
+							break;
+						case 'A':
+							content_type = "Audio";
+							overlay_col = 'rgba(204, 204, 255, 0.3)';
+							break;
+						case 'C':
+							content_type = "Code Snippet";
+							overlay_col = 'rgba(204, 255, 204, 0.3)';
+							break;
+						case 'L':
+							content_type = "Link";
+							overlay_col = 'rgba(204, 204, 255, 0.3)';
+							break;
+					}
+					var created_at = spark.get('created_at');
+
+					//Generate the inner html of the lightbox:
+					var sparkContent = '<h3>Spark</h3>'
+						+'<p>'+content_type+'</p>'
+						+'<p>Created on '+created_at+'</p>'
+						+'<button id="addToIdeaBucket">Add to Idea Bucket</button>'
+						+'<hr>'
+						+body
+						+'<hr>'
+						+'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>';
+
+					//Create FancyBox of Spark Details:
+					$.fancybox({
+						type: 'iframe',
+						helpers: {
+							overlay: {
+								css: {
+									'background' : overlay_col
+								}
+							}
+						},
+						content: sparkContent
+					});
+
+					$("#addToIdeaBucket").click(function(){
+						app.IdeaBucket.addSpark(id);	
+					});
+				}.bind(this)
+			});
 		}
 	});
 })();
